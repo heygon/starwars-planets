6cf90d9b6aa235b51e494780c5994397b2917602
Heygon Lago <dryvos@gmail.com>
2022-12-19 23:47:23 -0300

    Init planets details

--- src/app/star-wars/planet-list/planet-list.component.ts
+++ src/app/star-wars/planet-list/planet-list.component.ts
@@ -5,11 +5,90 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './planet-list.component.html',
   styleUrls: ['./planet-list.component.scss']
 })
-export class PlanetListComponent implements OnInit {
+export class PlanetListComponent  {
 
   constructor() { }
 
+  public planets = [
+    {
+      id     : '1',
+      image  : '../../../assets/img/Tatooine.webp',
+      name   : 'Tatoine',
+      resume : 'Tatooine was a desert world and the first planet in the binary Tatoo star system...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+    {
+      id     : '2',
+      image  : '../../../assets/img/kamino.webp',
+      name   : 'Kamino',
+      resume : 'Kamino also known as the Planet of Storms, was the watery world where the Clone Army of the Galactic Republic was created, as well as the Kamino Home Fleet...'
+      
+    },
+  ]
+
   ngOnInit(): void {
+
+
+
+
+
+
   }
 
 }
